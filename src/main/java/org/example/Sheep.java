c00c2c51e8e2ffca57c4a5c699caeca3ab08b793
John <ashton.basile@gmail.com>
2023-09-20 13:48:13 +0100

    I wrote the Sheep class which extends the FarmAnimal class

--- src/main/java/org/example/Sheep.java
+++ src/main/java/org/example/Sheep.java
@@ -3,5 +3,18 @@ package org.example;
 //in constructor, make species ‘sheep’
 //        an implementation of the makeANoise which returns an appropriate String (eg. baa)
 //        override of introduceYourself returning 'Hi, I am a sheep, baa baa'. Note that species and noise should come from variable.
-public class Sheep {
+public class Sheep extends FarmAnimal{
+    public Sheep(String species) {
+        super(species);
+    }
+
+    @Override
+    public String makeNoise() {
+        return "baa";
+    }
+
+    @Override
+    public String introduceSelf() {
+        return "Hi, I am a " + this.getSpecies() + ", " + this.makeNoise() + " " + this.makeNoise();
+    }
 }
